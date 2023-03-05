4e97a3d5e66ecae80bab134a9b2be343b87995c1
Mayo1590 <2035059@cgmatane.qc.ca>
2023-03-05 15:32:17 -0500

    projet détail

--- single.php
+++ single.php
@@ -1,15 +1,9 @@
 <?php get_header(); ?>
 
 <div class="contenu">
-    <div class="projet">
+    <div class="projet projet-detail">
                 <h3><?php the_title(); ?></h3>
                 <?php the_content(); ?>
             </div>
 
 <?php get_footer(); ?>
-
-<style>
-    .projet {
-        margin-top: 2%;
-    }
-</style>
